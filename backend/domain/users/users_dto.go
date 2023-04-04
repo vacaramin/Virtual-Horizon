09adaff1c468ac5dcc375c1b361fa91b01c8bae6
Waqar Amin <vacaramin86@gmail.com>
2023-04-05 04:15:32 +0500

    new users dao added

--- backend/domain/users/users_dto.go
+++ backend/domain/users/users_dto.go
@@ -1,9 +1,28 @@
 package users
 
+import (
+	"Virtual-Horizon/backend/utils/errors"
+	"strings"
+)
+
 type User struct {
 	ID        int64  `json:"ID"`
 	FirstName string `json:"first-name"`
 	LastName  string `json:"last-name"`
 	Password  string `json:"password"`
 	Email     string `json:"email"`
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.FirstName = strings.TrimSpace(user.FirstName)
+	user.LastName = strings.TrimSpace(user.LastName)
+	user.Email = strings.TrimSpace(user.Email)
+	if user.Email == "" {
+		return errors.NewBadRequestError("Invalid Email Address")
+	}
+	user.Password = strings.TrimSpace(user.Password)
+	if user.Password == "" {
+		return errors.NewBadRequestError("Invalid Password")
+	}
+	return nil
 }
--- backend/services/users_service.go
+++ backend/services/users_service.go
@@ -3,8 +3,18 @@ package services
 import (
 	"Virtual-Horizon/backend/domain/users"
 	"Virtual-Horizon/backend/utils/errors"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
-
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+	// encrypt the Password
+	pwSlice, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return nil, errors.NewBadRequestError("failed to encrypt password")
+	}
+	user.Password = string(pwSlice[:])
+	user.Save()
 }
