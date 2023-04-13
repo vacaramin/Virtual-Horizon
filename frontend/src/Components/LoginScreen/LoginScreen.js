5497efe2697331deac06e7c9d563da3aad2e936b
Waqar Amin <vacaramin86@gmail.com>
2023-04-13 06:05:01 +0500

    Frontend of Login Page implemented in ReactJS

---
+++ frontend/src/Components/LoginScreen/LoginScreen.js
@@ -0,0 +1,27 @@
+
+import './LoginScreen.css';
+
+function LoginScreen() {
+    return (
+        <div className="LoginScreen">
+            <div className='left-rectangle'></div>
+
+            <div className='right-image'></div>
+
+            <div className='signinButton' />
+            <div >
+                <div className='EmailText'>Email</div>
+                <div className='EmailText'>Email</div>
+                <div className='signinText'>SignIn</div>
+                <div className='passwordText'>Password</div>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='signinBoxText'>SignIn</div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default LoginScreen;
