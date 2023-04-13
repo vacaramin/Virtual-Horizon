6058533cf2c871fcbeaf676d969967eea1f2fd25
Waqar Amin <vacaramin86@gmail.com>
2023-04-13 06:05:01 +0500

    Frontend of Login Page implemented in ReactJS

---
+++ frontend/src/Components/SignupScreen/SignupScreen.js
@@ -0,0 +1,14 @@
+
+import './SignupScreen.css';
+
+function SignupScreen() {
+  return (
+    <div className="SignupScreen">
+     <div className='left-rectangle'></div>
+     
+     <div className='right-image'></div>
+    </div>
+  );
+}
+
+export default SignupScreen;
---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignupScreen from './Components/SignupScreen/SignupScreen';
+import LoginScreen from './Components/LoginScreen/LoginScreen';
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/signup' element={<SignupScreen />} />
+        <Route exact path='/login' element={<LoginScreen />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
