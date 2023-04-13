b8b3b03cee7c9c6949fe433e39d3e2b3059aa702
Waqar Amin <vacaramin86@gmail.com>
2023-04-13 08:06:10 +0500

    Sign up page added

--- frontend/src/Components/SignupScreen/SignupScreen.js
+++ frontend/src/Components/SignupScreen/SignupScreen.js
@@ -7,6 +7,19 @@ function SignupScreen() {
      <div className='left-rectangle'></div>
      
      <div className='right-image'></div>
+
+    <div className='signUpButton'></div>
+    <div className='signUptext'>SignUp</div>
+    <div className='nameText'>Name</div>
+    <div className='signupButtonText'>SignUp</div>
+    <div className='emailText'>Email</div>
+    <div className='line1'></div>
+    <div className='line2'></div>
+    <div className='password'>Password</div>
+    <div className='contactNumberText'>Contact</div>
+    <div className='line3'></div>
+    <div className='line4'></div>
+    
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import LoginScreen from './Components/LoginScreen/LoginScreen';
+//import App from './App';
+//import LoginScreen from './Components/LoginScreen/LoginScreen';
+import SignupScreen from './Components/SignupScreen/SignupScreen';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginScreen/>
+    <SignupScreen/>
   </React.StrictMode>
 );
 
