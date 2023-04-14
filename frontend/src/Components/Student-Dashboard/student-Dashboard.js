0244c5c2b5112bb67a77f3989ce8d28eef6ead99
Waqar Amin <vacaramin86@gmail.com>
2023-04-14 11:56:43 +0500

    Student Dashboard, Components addition

---
+++ frontend/src/Components/Student-Dashboard/student-Dashboard.js
@@ -0,0 +1,12 @@
+
+import './student-Dashboard.css';
+import Header from './Header/Header';
+function studentDashboard() {
+  return (
+    <div className="Header">
+     <Header></Header>
+    </div>
+  );
+}
+
+export default studentDashboard;
