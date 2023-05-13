06a0551cbae3a8ecebf11cda5341b8b297ad16c6
Waqar Amin <vacaramin86@gmail.com>
2023-05-13 14:35:24 +0500

    routing has been setup

--- frontend/src/Components/Student-Dashboard/Header/Header.js
+++ frontend/src/Components/Student-Dashboard/Header/Header.js
@@ -1,12 +1,10 @@
-
+import React from 'react';
 import './Header.css';
 
 function Header() {
   return (
-    <div className="Header">
-     <div className= 'logo'></div>
-     <div className = 'student_name'>Hello, Mr Awais</div>
-     <div className='student_dp'></div>
+    <div>
+     
     </div>
   );
 }
