e4219972a9f70580f8e13d8e59469490a2d429e1
Waqar Amin <vacaramin86@gmail.com>
2023-06-11 03:44:04 +0500

    subjects partly implemented

--- frontend/src/Components/StudentDashboard/ContentArea/ContentArea.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/ContentArea.jsx
@@ -1,13 +1,146 @@
-import React from 'react';
+import React from "react";
+import './ContentArea.css';
+import { useState } from "react";
+function ContentArea({selectedItem}) {
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [subjectSelected, setSubjectSelected] = useState(false);
 
-function ContentArea() {
-  return (
-    <div className="content-area">
-      <h1>Welcome to the student dashboard</h1>
-      <p>Work Under Progress...</p>
-      <img style = {{width: "300px"}}src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg" alt="s" />
-    </div>
-  );
+  const subjects = [
+    { name: "Math", color: "#FF5B5B", content: "Math Content" },
+    { name: "Science", color: "#3EC1D3", content: "Science Content" },
+    { name: "History", color: "#FFC870", content: "History Content" },
+    { name: "Art", color: "#C5C3FF", content: "Art Content" },
+    { name: "Music", color: "#FF94C2", content: "Music Content" },
+    { name: "Physical Education", color: "#A5D296", content: "PE Content" },
+  ];
+
+  const handleSubjectClick = (subject) => {
+    setSelectedSubject(subject);
+    setSubjectSelected(true);
+  };
+
+  const handleGoBack = () => {
+    setSelectedSubject(null);
+    setSubjectSelected(false);
+  };
+
+  if (selectedItem === "home") {
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p> Home *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  } else if (selectedItem === "subjects") {
+    if (subjectSelected && selectedSubject) {
+      return (
+        <div className="content-area">
+          <h1>{selectedSubject.name}</h1>
+          <p>{selectedSubject.content}</p>
+          <button onClick={handleGoBack}>Go Back</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="content-area">
+          <h1>Subjects</h1>
+          <div className="subject-container">
+            {subjects.map((subject) => (
+              <span
+                onClick={() => handleSubjectClick(subject)}
+                key={subject.name}
+                className="subject-item"
+                style={{ backgroundColor: subject.color }}
+              >
+                {subject.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      );
+    }
+  } else if (selectedItem === "timetable") {
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p>Time Table *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  }
+  else if (selectedItem === "tutor") {
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p> Tutor *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  }
+  else if (selectedItem === "classlinks") {
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p> Class Links *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  }
+  else if (selectedItem === "reportcard") {
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p> Report Card *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  }
+  else if (selectedItem === "settings") {
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p>Settings *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  } else{
+    return (
+      <div className="content-area">
+        <h1>Welcome to the student dashboard</h1>
+        <p>Default *** Work Under Progress...</p>
+        <img
+          style={{ width: "300px" }}
+          src="https://static.vecteezy.com/system/resources/previews/002/315/143/original/under-construction-symbol-sign-free-vector.jpg"
+          alt="s"
+        />
+      </div>
+    );
+  }
 }
 
 export default ContentArea;
--- frontend/src/Components/StudentDashboard/Sidebar/Sidebar.jsx
+++ frontend/src/Components/StudentDashboard/Sidebar/Sidebar.jsx
@@ -9,8 +9,8 @@ import classlinks from './Logo of Student Dashboard/Class-links.svg'
 import reportcard from './Logo of Student Dashboard/Report-Card.svg'
 import settings from './Logo of Student Dashboard/Setting.svg'
 function Sidebar(props) {
-  const { isSidebarOpen, toggleSidebar } = props;
-
+  const { isSidebarOpen, toggleSidebar, handleItemClick, selectedItem} = props;
+  
   return (
     <div className={`sidebar-menu ${isSidebarOpen ? "" : "collapsed"}`}>
       <div className="sidebar-header">
@@ -20,35 +20,35 @@ function Sidebar(props) {
       </div>
 
       <div className="sidebar-items">
-        <div className="sidebar-item">
+        <div className={`sidebar-item ${selectedItem === "home" ? "selected" : ""}`} onClick={ () => handleItemClick('home')}>
           <img src={home} alt="Home icon" />
           {isSidebarOpen && <span>Home</span>}
         </div>
-        <div className="sidebar-item">
-          <img src={subjecticon} alt="Settings icon" />
+        <div className={`sidebar-item ${selectedItem === "subjects" ? "selected" : ""}`} onClick = {() => handleItemClick('subjects')}>
+          <img src={subjecticon} alt="Subjects icon" />
           {isSidebarOpen && <span>Subjects</span>}
         </div>
 
-        <div className="sidebar-item">
+        <div className={`sidebar-item ${selectedItem === "timetable" ? "selected" : ""}`} onClick = {() => handleItemClick('timetable')}>
           <img src={timetable} alt="Settings icon" />
           {isSidebarOpen && <span>Time Table</span>}
         </div>
 
-        <div className="sidebar-item">
+        <div className={`sidebar-item ${selectedItem === "tutor" ? "selected" : ""}`} onClick={ () => handleItemClick('tutor')}>
           <img src={Tutor} alt="Settings icon" />
           {isSidebarOpen && <span>Tutor</span>}
         </div>
 
-        <div className="sidebar-item">
+        <div className={`sidebar-item ${selectedItem === "classlinks" ? "selected" : ""}`} onClick = {() => handleItemClick('classlinks')}>
           <img src={classlinks} alt="Settings icon" />
           {isSidebarOpen && <span>Class Links</span>}
         </div>
 
-        <div className="sidebar-item">
+        <div className={`sidebar-item ${selectedItem === "reportcard" ? "selected" : ""}`} onClick = {() => handleItemClick('reportcard')}>
           <img src={reportcard} alt="Settings icon" />
           {isSidebarOpen && <span>Report Card</span>}
         </div>
-        <div className="sidebar-item">
+        <div className={`sidebar-item ${selectedItem === "settings" ? "selected" : ""}`} onClick={() => handleItemClick('settings')}>
           <img src={settings} alt="Settings icon" />
           {isSidebarOpen && <span>Settings</span>}
         </div>
