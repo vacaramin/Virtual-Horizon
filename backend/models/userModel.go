108af7b9a1bea6db7f59d5d17ed901f0ec3320c1
Waqar Amin <vacaramin86@gmail.com>
2023-08-07 02:09:12 +0500

    User Model Updated for sign up

--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -1,10 +1,26 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique"`
-	Password string
-	Name     string
+	Email               string `gorm:"unique"`
+	Password            string
+	Name                string
+	Dob                 time.Time
+	Gender              string
+	ParentGuardianName  string
+	ParentGuardianEmail string
+	ParentGuardianPhone string
+	GradeLevel          string
+	CurrentSchool       string
+	Device              string
+	InternetConnection  string
+	SpecialNeeds        string
+	Accomodations       string
+	PresentAddress      string
 }
