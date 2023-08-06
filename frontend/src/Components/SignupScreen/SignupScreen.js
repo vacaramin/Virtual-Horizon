280d4177425ad50e0c419deb4e871a86e2ba335c
Waqar Amin <vacaramin86@gmail.com>
2023-08-07 03:06:48 +0500

    sign up screen setup

--- frontend/src/Components/SignupScreen/SignupScreen.js
+++ frontend/src/Components/SignupScreen/SignupScreen.js
@@ -15,31 +15,7 @@ function SignupScreen() {
                 <br/>
                 
                 <SignupForm class = "sign-up-form" />
-                {/* 
-
-                <div style={{ display: "block" }}>
-
-                    <input className='input-name' id="name" placeholder='Name' />
-                    <br />
-                    <input className='input-email' id="email" placeholder='email'/>
-     
-                    <br />
-                    
-                    <input className='input-pw' id="password"  type = 'password' placeholder='password'/>
-                    
-                    <br />
-                    <input className='input-contact' id="Contact" placeholder='Contact Number'/>
                 
-                </div>
-                <div>
-
-                    <Link to="/login-student">
-                        <div className='signinButton'>
-                            Sign Up
-                        </div>
-                    </Link>
-
-                </div> */}
 
                 <div className='sign-up-text'><i style={{ textAlign: "center" }}>Already Registered? <Link to="/login-student">Login</Link></i></div>
 
