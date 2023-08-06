c7bdc84c21efaa2f7f91138e7264cf457e15be3c
Waqar Amin <vacaramin86@gmail.com>
2023-08-07 03:06:48 +0500

    sign up screen setup

--- frontend/src/Components/SignupScreen/SignupForm/SignUpForm.jsx
+++ frontend/src/Components/SignupScreen/SignupForm/SignUpForm.jsx
@@ -3,6 +3,21 @@ import "./SignupForm.css";
 import { styled } from "@mui/system";
 import { Stepper, Step, StepLabel } from "@mui/material";
 
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [Name, setName] = useState("");
+const [Dob, setDob] = useState(""); // Assuming you'll handle date using a date picker
+const [Gender, setGender] = useState("");
+const [ParentGuardianName, setParentGuardianName] = useState("");
+const [ParentGuardianEmail, setParentGuardianEmail] = useState("");
+const [ParentGuardianPhone, setParentGuardianPhone] = useState("");
+const [GradeLevel, setGradeLevel] = useState("");
+const [CurrentSchool, setCurrentSchool] = useState("");
+const [Device, setDevice] = useState("");
+const [InternetConnection, setInternetConnection] = useState("");
+const [SpecialNeeds, setSpecialNeeds] = useState("");
+const [Accomodations, setAccomodations] = useState("");
+const [PresentAddress, setPresentAddress] = useState("");
 
 import {
   Button,
@@ -13,7 +28,6 @@ import {
   RadioGroup,
   Radio,
 } from "@mui/material";
-
 
 const Container = styled("div")({
   width: "80%",
@@ -29,7 +43,7 @@ const StepperContainer = styled("div")({
 
 const BackButton = styled(Button)({
   marginRight: "15%",
-  background: '#243047',
+  background: "#243047",
   borderRadius: "76px",
   width: "20%",
   color: "white",
@@ -45,7 +59,7 @@ const BackButton = styled(Button)({
 
 const NextButton = styled(Button)({
   marginRight: "0%",
-  background: '#243047',
+  background: "#243047",
   borderRadius: "76px",
   color: "white",
   width: "50%",
@@ -60,6 +74,23 @@ const Instructions = styled(Typography)({
   marginBottom: "20%",
 });
 
+const payload = {
+  email,
+  password,
+  Name,
+  Dob,
+  Gender,
+  ParentGuardianName,
+  ParentGuardianEmail,
+  ParentGuardianPhone,
+  GradeLevel,
+  CurrentSchool,
+  Device,
+  InternetConnection,
+  SpecialNeeds,
+  Accomodations,
+  PresentAddress,
+};
 
 const steps = [
   "Personal Information",
@@ -69,7 +100,6 @@ const steps = [
   "Additional Information",
   "Present Address",
 ];
-
 
 const SignupForm = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -93,12 +123,33 @@ const SignupForm = () => {
       case 0:
         return (
           <div>
-            <TextField id="name" label="Name" fullWidth style={inputSpacing} />
+            <TextField
+              id="name"
+              label="Name"
+              fullWidth
+              style={inputSpacing}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              id="email"
+              label="email"
+              fullWidth
+              style={inputSpacing}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              id="password"
+              label="Password"
+              fullWidth
+              style={inputSpacing}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <TextField
               id="dateOfBirth"
               label="Date of Birth"
               fullWidth
               style={inputSpacing}
+              onChange={(e) => setDob(e.target.value)}
             />
             <FormControl component="fieldset" style={inputSpacing}>
               <RadioGroup row aria-label="gender">
@@ -106,11 +157,13 @@ const SignupForm = () => {
                   value="male"
                   control={<Radio />}
                   label="Male"
+                  onChange={(e) => setGender(e.target.value)}
                 />
                 <FormControlLabel
                   value="female"
                   control={<Radio />}
                   label="Female"
+                  onChange={(e) => setGender(e.target.value)}
                 />
               </RadioGroup>
             </FormControl>
@@ -124,57 +177,87 @@ const SignupForm = () => {
               label="Parent/Guardian's Name"
               fullWidth
               style={inputSpacing}
+              onChange={(e) => setParentGuardianName(e.target.value)}
             />
             <TextField
               id="parentEmail"
               label="Parent/Guardian's Email"
               fullWidth
               style={inputSpacing}
+              onChange={(e) => setParentGuardianEmail(e.target.value)}
             />
             <TextField
               id="parentPhone"
               label="Parent/Guardian's Phone Number"
               fullWidth
               style={inputSpacing}
+              onChange={(e) => setParentGuardianPhone(e.target.value)}
             />
           </div>
         );
       case 2:
         return (
           <div>
-            <TextField id="gradeLevel" label="Current Grade Level" fullWidth />
+            <TextField
+              id="gradeLevel"
+              label="Current Grade Level"
+              fullWidth
+              onChange={(e) => setGradeLevel(e.target.value)}
+            />
             <br />
             <TextField
               id="schoolName"
               label="Name of Current School"
               fullWidth
+              onChange={(e) => setCurrentSchool(e.target.value)}
             />
           </div>
         );
       case 3:
         return (
           <div>
-            <TextField id="device" label="Device" fullWidth />
+            <TextField
+              id="device"
+              label="Device"
+              fullWidth
+              onChange={(e) => setDevice(e.target.value)}
+            />
             <br />
             <TextField
               id="internetConnection"
               label="Internet Connection"
               fullWidth
+              onChange={(e) => setInternetConnection(e.target.value)}
             />
           </div>
         );
       case 4:
         return (
           <div>
-            <TextField id="specialNeeds" label="Special Needs" fullWidth />
+            <TextField
+              id="specialNeeds"
+              label="Special Needs"
+              fullWidth
+              onChange={(e) => setSpecialNeeds(e.target.value)}
+            />
             <br />
-            <TextField id="accommodations" label="Accommodations" fullWidth />
+            <TextField
+              id="accommodations"
+              label="Accommodations"
+              fullWidth
+              onChange={(e) => setAccomodations(e.target.value)}
+            />
           </div>
         );
       case 5:
         return (
           <div>
-            <TextField id="presentAddress" label="Present Address" fullWidth />
+            <TextField
+              id="presentAddress"
+              label="Present Address"
+              fullWidth
+              onChange={(e) => setPresentAddress(e.target.value)}
+            />
           </div>
         );
       default:
@@ -188,9 +271,7 @@ const SignupForm = () => {
         <Stepper activeStep={activeStep} alternativeLabel>
           {steps.map((label, index) => (
             <Step key={label}>
-              <StepLabel>
-                {label}
-              </StepLabel>
+              <StepLabel>{label}</StepLabel>
             </Step>
           ))}
         </Stepper>
@@ -204,7 +285,14 @@ const SignupForm = () => {
             <Button
               variant="contained"
               color="primary"
-              onClick={() => setActiveStep(0)}
+              onClick={() => {
+                setActiveStep(0);
+                fetch("https://localhost:4000/signup", {
+                  method: "POST",
+                  mode: "cors",
+                  body: JSON.stringify(payload),
+                });
+              }}
             >
               Reset
             </Button>
