b6852ad6de79b1668a1110d33ff435118eedbe48
Waqar Amin <vacaramin86@gmail.com>
2023-08-07 05:15:28 +0500

    Get user details from ID set

--- backend/main.go
+++ backend/main.go
@@ -27,7 +27,7 @@ func main() {
 	r.POST("/signup", controllers.Signup)
 	r.POST("/login", controllers.Login)
 	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
-
+	r.GET("/GetProfileByID/:ID", middleware.RequireAuth, controllers.GetProfileByID)
 	r.Run()
 	fmt.Println("Hello, world!")
 }
