3745c50cf42d53cbb5521010e33d83df14120fb6
Waqar Amin <vacaramin86@gmail.com>
2023-08-10 03:30:57 +0500

    user controller

--- backend/controllers/userController.go
+++ backend/controllers/userController.go
@@ -144,6 +144,7 @@ func Login(c *gin.Context) {
 		"status":   "success",
 		"token":    tokenString,
 		"username": user.Name,
+		"user":     user,
 	})
 
 }
@@ -189,4 +190,37 @@ func GetProfileByID(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"user": user,
 	})
+}
+func GetIdfromToken(c *gin.Context) {
+	tokenString, err := c.Cookie("Authorization")
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	//Decode/Validate it
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok { //check the signing method
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		//hmacSampl
+		return []byte(os.Getenv("SECRET")), nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		//check exp
+		if time.Now().Unix() > int64(claims["exp"].(float64)) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		//find the user with token sub
+		var user models.User
+		initializers.DB.First(&user, claims["sub"])
+		if user.ID == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+		c.JSON(http.StatusNotFound, gin.H{
+			"user": user},
+		)
+
+	}
 }
