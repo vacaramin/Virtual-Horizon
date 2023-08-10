720c0572b22d59773889db65ae76f55837fe4ee6
Waqar Amin <vacaramin86@gmail.com>
2023-08-11 04:44:43 +0500

    bug in middleware

--- frontend/src/Components/StudentDashboard/ContentArea/Settings/Settings.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/Settings/Settings.jsx
@@ -1,77 +1,76 @@
 import React, { useState, useEffect } from "react";
 import "./Settings.css";
 import Container from "./Container/Container";
-import { List, ListItem, ListItemText } from '@mui/material';
-
+import { List, ListItem, ListItemText } from "@mui/material";
 
 function Settings() {
   const [editing, setEditing] = useState(false);
-  const data = [
-    { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User' },
-    { id: 3, name: 'Bob Johnson', email: 'bob@example.com', role: 'User' },
-    { id: 4, name: 'Alice Brown', email: 'alice@example.com', role: 'Admin' },
-  ];
+  const [user, setUser] = useState({});
+  console.log(document.cookie)
   useEffect(() => {
-    fetch("http://localhost:4000/GetProfileByID/5")
+    fetch("http://localhost:4000/GetProfileByID/5", {method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
       .then(response => response.json())
       .then(data => {
         setUser(data.user);
         console.log(data.user);
+        setUser(data.user)
       })
       .catch(error => {
         console.error("Error fetching user data:", error);
       });
   }, []);
 
-
-  // const handleLogin_Teacher = () => {
-  //   const payload = { email, password };
-  //   fetch("http://localhost:4000/GetProfileByID/5", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(payload),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       //if responsce is ok
-  //       if (data.status === "success") {
-  //         console.log("Login Successful");
-  //         //strong the recieving token in data to local browser cookies
-  //         localStorage.setItem("token", data.token);
-  //         localStorage.setItem("username", data.username);
-          
-  //         //setting cookie authorization token
-  //         document.cookie = `authorization=${data.token}`;
-  //         document.cookie = `username=${data.username}`;
-  //         //redirect to home page
-  //         history("/home-teacher");
-
-  //         //redirect to home page
-  //       }
-  //       if (data.status === "failed") {
-  //         console.log("Login Failed");
-          
-  //         console.log(data);
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-
   return (
-    
-      
     <Container>
-
-    <List>
-      {data.map((item) => (
-        <ListItem key={item.name}>
-          <ListItemText primary={item.email} secondary={item.role} />
+      <List>
+        <ListItem>
+          <ListItemText primary={`Name: ${user.Name}`} />
         </ListItem>
-      ))}
-    </List>
+        <ListItem>
+          <ListItemText primary={`Email: ${user.Email}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Date of Birth: ${user.Dob}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Gender: ${user.Gender}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Parent/Guardian Name: ${user.ParentGuardianName}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Parent/Guardian Email: ${user.ParentGuardianEmail}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Parent/Guardian Phone: ${user.ParentGuardianPhone}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Grade Level: ${user.GradeLevel}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Current School: ${user.CurrentSchool}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Device: ${user.Device}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Internet Connection: ${user.InternetConnection}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Special Needs: ${user.SpecialNeeds}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Accommodations: ${user.Accommodations}`} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={`Present Address: ${user.PresentAddress}`} />
+        </ListItem>
+      </List>
     </Container>
   );
 }
