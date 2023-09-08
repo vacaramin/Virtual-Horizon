fc6888a157721cc712175350e0a6023532f98541
Waqar Amin <68877880+vacaramin@users.noreply.github.com>
2023-09-09 04:16:41 +0500

    Removed Frontend Waste

--- frontend/src/Components/StudentDashboard/ContentArea/ContentArea.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/ContentArea.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./ContentArea.css";
 import { useState } from "react";
-import Timetable from "./TimeTable/TimeTable";
-import ClassLinks from "./ClassLinks/ClassLinks";
-import ReportCard from "./ReportCard/ReportCard";
 import Settings from "./Settings/Settings";
 import Home from "./Home/Home";
 import Tutor from "./Tutor/Tutor";
@@ -73,48 +70,6 @@ function ContentArea({ selectedItem }) {
         </div>
       );
     }
-  } else if (selectedItem === "timetable") {
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    const subjects = ["Math", "Science", "History", "Art", "Music"];
-
-    function generateRandomTimetable() {
-      const slots = [];
-      const times = ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM"];
-      const numSlots = times.length;
-
-      for (let i = 0; i < numSlots; i++) {
-        const time = times[i];
-        const slotSubjects = [];
-
-        for (let j = 0; j < days.length; j++) {
-          const day = days[j];
-          const randomSubjectIndex = Math.floor(
-            Math.random() * subjects.length
-          );
-          const subject = subjects[randomSubjectIndex];
-          slotSubjects.push({ day, name: subject });
-        }
-
-        slots.push({ time, subjects: slotSubjects });
-      }
-
-      return slots;
-    }
-
-    const timetable = {
-      days,
-      slots: generateRandomTimetable(),
-    };
-
-    return (
-      <div>
-        <h1>TimeTable</h1>
-
-        <div className="content-area-student">
-          <Timetable days={days} subjects={subjects} timetable={timetable} />
-        </div>
-      </div>
-    );
   } else if (selectedItem === "tutor") {
     return (
       <div>
@@ -123,53 +78,6 @@ function ContentArea({ selectedItem }) {
         <Tutor/>
         
       </div>
-      </div>
-    );
-  } else if (selectedItem === "classlinks") {
-    const classLinks = [
-      { subject: "Math", url: "https://math-class-link.com" },
-      { subject: "Science", url: "https://science-class-link.com" },
-      { subject: "History", url: "https://history-class-link.com" },
-    ];
-    return (
-      <div>
-        <h1>Class Links</h1>
-
-        <div className="content-area-student">
-          <ClassLinks links={classLinks} />
-        </div>
-      </div>
-    );
-  } else if (selectedItem === "reportcard") {
-    const dummyReportCardData = {
-      studentName: "John Doe",
-      subjects: [
-        {
-          name: "Math",
-          quizzes: ["Quiz 1", "Quiz 2"],
-          assignments: ["Assignment 1", "Assignment 2"],
-          midExam: 85,
-          finalExam: 92,
-        },
-        {
-          name: "Science",
-          quizzes: ["Quiz 1", "Quiz 2", "Quiz 3"],
-          assignments: ["Assignment 1", "Assignment 2", "Assignment 3"],
-          midExam: 78,
-          finalExam: 88,
-        },
-        // Add more subjects here...
-      ],
-    };
-    return (
-      <div>
-        <h1>Report Card</h1>
-        <div className="content-area-student">
-          <ReportCard
-            studentName={dummyReportCardData.studentName}
-            subjects={dummyReportCardData.subjects}
-          />
-        </div>
       </div>
     );
   } else if (selectedItem === "settings") {
