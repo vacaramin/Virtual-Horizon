1351bce8607583a7f4bf0b7556c4e9e5e728e765
Awais Mohammad <awaismadad@gmail.com>
2023-09-10 02:30:58 +0500

    added content

---
+++ frontend/src/Components/StudentDashboard/ContentArea/LanguageSupport/LanguageSupport.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LanguageSupport.css';
+
+function LanguageSupport() {
+  return (
+    <div className="LanguageSupport-Container">
+      <div className="topbar"> Language fjksdhfskdjhfskdjfhsdkjfhsdkjsfhSupport
+      <div className='languages'>ksjdhfskdjhskdjhf</div>
+      </div>
+    </div>
+  );
+}
+
+export default LanguageSupport;
