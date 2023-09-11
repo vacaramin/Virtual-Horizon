7b7dc7bef99e8574b285b241a10d04bb89741cce
Waqar Amin <68877880+vacaramin@users.noreply.github.com>
2023-09-11 23:09:56 +0500

    down migration changed to new ERD

--- backend/Migrations/001_initial.down.sql
+++ backend/Migrations/001_initial.down.sql
@@ -1,18 +1,3 @@
--- Drop the "class_participants" table
-DROP TABLE IF EXISTS class_participants;
-
--- Drop the "messages" table
-DROP TABLE IF EXISTS messages;
-
--- Drop the "files" table
-DROP TABLE IF EXISTS files;
-
--- Drop the "class" table
-DROP TABLE IF EXISTS classes;
-
--- Drop the "course" table
-DROP TABLE IF EXISTS courses;
-
 -- Drop the "tutors" table
 DROP TABLE IF EXISTS tutors;
 
