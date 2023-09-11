d1e432bd1ae7c53d0be35e9f3166454afba5c339
Waqar Amin <68877880+vacaramin@users.noreply.github.com>
2023-09-11 23:09:28 +0500

    up migration initialized

--- backend/Migrations/001_initial.up.sql
+++ backend/Migrations/001_initial.up.sql
@@ -1,5 +1,5 @@
 -- File: 001_initial.up.sql
--- Create the "user" table
+-- Create the "users" table
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
@@ -8,149 +8,45 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(255) NOT NULL,
     dob TIMESTAMPTZ,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    role VARCHAR(10) -- Add the role column
+    );
 
--- Create the "student" table
+-- Create the "students" table
 CREATE TABLE IF NOT EXISTS students (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
-    parent_guardian_name VARCHAR(255),
-    parent_guardian_email VARCHAR(255),
-    parent_guardian_phone VARCHAR(20),
-    grade_level VARCHAR(255),
-    current_school VARCHAR(255),
-    device VARCHAR(255),
-    internet_connection VARCHAR(255),
-    special_needs TEXT,
-    accomodations TEXT,
-    present_address TEXT,
+    parent_guardian_name VARCHAR(255) NOT NULL,
+    parent_guardian_email VARCHAR(255) NOT NULL,
+    parent_guardian_phone VARCHAR(255) NOT NULL,
+    grade_level VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
-
--- Insert sample data into the "users" table
-INSERT INTO
-    users (email, password, gender, name, dob)
-VALUES
-    (
-        'vacaramin86@gmail.com',
-        '$2a$10$RoehRLxjbt1jHUT2j9Gihu6QYeRRa3xuycKs0GGDxpEb3dJFzo6ga',
-        'Male',
-        'Waqar Amin',
-        '1990-01-01'
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE
     );
 
--- Insert sample data into the "students" table
-INSERT INTO
-    students (
-        user_id,
-        parent_guardian_name,
-        parent_guardian_email,
-        parent_guardian_phone,
-        grade_level,
-        current_school,
-        device,
-        internet_connection,
-        special_needs,
-        accomodations,
-        present_address
-    )
-VALUES
-    (
-        1,
-        'John Doe',
-        'johndoe@example.com',
-        '1234567890',
-        '10th Grade',
-        'ABC High School',
-        'Laptop',
-        'High-speed',
-        'None',
-        'None',
-        '123 Main St'
-    );
-
--- Create the "tutor" table
+-- Create the "tutors" table
 CREATE TABLE IF NOT EXISTS tutors (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
-    department VARCHAR(255),
-    designation VARCHAR(255),
+    subject VARCHAR(255) NOT NULL,
+    experience VARCHAR(255) NOT NULL,
+    rating VARCHAR(10) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
-
--- Insert sample data into the "tutors" table
-INSERT INTO
-    tutors (user_id, department, designation)
-VALUES
-    (1, 'Mathematics', 'Senior tutor'),
-    (1, 'Science', 'Assistant Professor');
-
--- Create the "course" table
-CREATE TABLE IF NOT EXISTS courses (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    tutor_id INTEGER NOT NULL REFERENCES tutors(id),
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
-
--- Insert sample data into the "course" table
-INSERT INTO
-    courses (name, description, tutor_id)
-VALUES
-    (
-        'Mathematics 101',
-        'Introductory course on mathematics',
-        1
-    ),
-    (
-        'Chemistry 201',
-        'Advanced course on chemistry',
-        2
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE
     );
 
--- Create the "class" table
-CREATE TABLE IF NOT EXISTS classes (
-    id SERIAL PRIMARY KEY,
-    tutor_id INTEGER NOT NULL REFERENCES tutors(id),
-    subject VARCHAR(255),
-    schedule TIMESTAMPTZ,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+-- Insert sample users
+INSERT INTO users (email, password, gender, name, dob, role)
+VALUES
+    ('vacaramin86@gmail.com', '$2a$10$RoehRLxjbt1jHUT2j9Gihu6QYeRRa3xuycKs0GGDxpEb3dJFzo6ga', 'male', 'Waqar Amin', '1990-01-01', 'student'),
+    ('awaismadad@gmail.com', '$2a$10$RoehRLxjbt1jHUT2j9Gihu6QYeRRa3xuycKs0GGDxpEb3dJFzo6ga', 'female', 'Awais Madad', '1995-03-15', 'tutor');
 
--- Create the "class_participants" table
-CREATE TABLE IF NOT EXISTS class_participants (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
-    class_id INTEGER NOT NULL REFERENCES classes(id),
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+-- Insert sample students
+INSERT INTO students (id, parent_guardian_name, parent_guardian_email, parent_guardian_phone, grade_level)
+VALUES
+    (1, 'Parent One', 'parent1@example.com', '123456789', 'Grade 5');
 
--- Create the "messages" table
-CREATE TABLE IF NOT EXISTS messages (
-    id SERIAL PRIMARY KEY,
-    sender_id INTEGER NOT NULL REFERENCES users(id),
-    class_id INTEGER NOT NULL REFERENCES classes(id),
-    content TEXT,
-    timestamp TIMESTAMPTZ DEFAULT NOW(),
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
-
--- Create the "files" table
-CREATE TABLE IF NOT EXISTS files (
-    id SERIAL PRIMARY KEY,
-    uploader_id INTEGER NOT NULL REFERENCES users(id),
-    class_id INTEGER NOT NULL REFERENCES classes(id),
-    filename VARCHAR(255),
-    filepath VARCHAR(255),
-    timestamp TIMESTAMPTZ DEFAULT NOW(),
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
+-- Insert sample tutors
+INSERT INTO tutors (id, subject, experience, rating)
+VALUES
+    (2, 'Math', '5 years', '4.5');
