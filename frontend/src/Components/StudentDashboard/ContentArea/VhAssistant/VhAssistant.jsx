eb8b4f328413e79b87a94c61768396445c8e7092
Awais Mohammad <awaismadad@gmail.com>
2023-09-12 12:57:21 +0500

    Text field added in the VH Assistant

--- frontend/src/Components/StudentDashboard/ContentArea/VhAssistant/VhAssistant.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/VhAssistant/VhAssistant.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { TextField } from "@mui/material";
 import './VhAssistant.css';
 
 function VhAssistant() {
   return (
-    <div className="LanguageSupport-Container">
-      <div className="topbar"> Language fjksdhfskdjhfskdjfhsdkjfhsdkjsfhSupport
-      <div className='languages'>ksjdhfskdjhskdjhf</div>
+    <div>
+      <div className="topbar">Welcome to the Virtual Horizon Assistant. The VH Assistant is an AI based chatbot helping you in asking different sort of questions in order to resolve your queries.</div>
+      
+      <TextField
+            id="Input"
+            type="text"
+            label="Enter your query..."
+            style={{ marginTop: "10%", marginLeft: "40%" }}
+            halfWidth
+            
+          />
+
       </div>
-    </div>
+
+    
   );
 }
 
