b6fae10dbe3462ae32657226c4504b00c2de3d44
vacaramin <vacaramin86@gmail.com>
2023-09-15 12:25:38 +0500

    Migrations added for Course, tutor course, chat and resource

--- backend/Migrations/001_initial.down.sql
+++ backend/Migrations/001_initial.down.sql
@@ -1,3 +1,24 @@
+-- Drop the "resource_items" table
+DROP TABLE IF EXISTS resource_items;
+
+-- Drop the "resources" table
+DROP TABLE IF EXISTS resources;
+
+-- Drop the "messages" table
+DROP TABLE IF EXISTS messages;
+
+-- Drop the "chats" table
+DROP TABLE IF EXISTS chats;
+
+-- Drop the "enrollments" table
+DROP TABLE IF EXISTS enrollments;
+
+-- Drop the "courses" table
+DROP TABLE IF EXISTS courses;
+
+-- Drop the "tutor_courses" table
+DROP TABLE IF EXISTS tutor_courses;
+
 -- Drop the "tutors" table
 DROP TABLE IF EXISTS tutors;
 
