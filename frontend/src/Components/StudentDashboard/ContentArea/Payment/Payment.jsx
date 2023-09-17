4eb2ec3cc69531cc79e434aa44edb6e5d5a28209
Awais Mohammad <awaismadad@gmail.com>
2023-09-18 00:25:12 +0500

    Payment UI completed with the click listeners as the hovering funtionality added

--- frontend/src/Components/StudentDashboard/ContentArea/Payment/Payment.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/Payment/Payment.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import CreditCard from '/home/awais/Desktop/Virtual-Horizon/frontend/src/Components/StudentDashboard/ContentArea/Payment/Creditcard.svg'
-import Easypaisa from '/home/awais/Desktop/Virtual-Horizon/frontend/src/Components/StudentDashboard/ContentArea/Payment/easypaisa.svg'
+import CreditCard from '/home/awais/Desktop/Virtual-Horizon/frontend/src/Components/StudentDashboard/ContentArea/Payment/Creditcard.svg';
+import Easypaisa from '/home/awais/Desktop/Virtual-Horizon/frontend/src/Components/StudentDashboard/ContentArea/Payment/easypaisa.svg';
 
+// Create the PaymentCard component
+const PaymentCard = ({ iconUrl, title, onClick }) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-const PaymentCard = ({ iconUrl, title }) => {
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <Card sx={{ width: "40%"}}>
+    <Card
+      sx={{
+        width: '40%',
+        transition: 'transform 0.2s',
+        transform: isHovered ? 'scale(1.05)' : 'scale(1)',
+        cursor: 'pointer',
+        boxShadow: isHovered ? '0px 0px 10px 0px rgba(0,0,0,0.5)' : 'none',
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={onClick}
+    >
       <CardMedia
         component="img"
         height="250"
@@ -25,18 +46,28 @@ const PaymentCard = ({ iconUrl, title }) => {
   );
 };
 
-//payment function which will return the payment cards.
 function Payment() {
-  return (
+  const handleCreditCardClick = () => {
+    // Handle click for Payment via credit card
+    alert('Payment via credit card clicked');
+  };
 
-    <div style={{ display: 'flex', justifyContent: 'space-around'}}>
+  const handleEasypaisaClick = () => {
+    // Handle click for Payment via Easypaisa
+    alert('Payment via Easypaisa clicked');
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
       <PaymentCard
-        iconUrl={CreditCard} // Replace with the actual URL of your credit card icon
+        iconUrl={CreditCard} // Pass the imported SVG as the iconUrl
         title="Payment via credit card"
+        onClick={handleCreditCardClick}
       />
-      <PaymentCard lassName="content-area-student"
-        iconUrl= {Easypaisa} // Replace with the actual URL of your Easypaisa icon
+      <PaymentCard
+        iconUrl={Easypaisa} // Pass the imported SVG as the iconUrl
         title="Payment via Easypaisa"
+        onClick={handleEasypaisaClick}
       />
     </div>
   );
