2ac947b6f6f046d6db294493d53dcfe138c0d7d6
vacaramin <vacaramin86@gmail.com>
2023-09-18 10:28:38 +0500

    Course_tutor link added to models and controller function

--- backend/src/course/controllers/course.controller.go
+++ backend/src/course/controllers/course.controller.go
@@ -1,51 +1,16 @@
 package controllers
 
 import (
-	"Virtual-Horizon/initializers"
-	"Virtual-Horizon/src/course/models"
-	usermodel "Virtual-Horizon/src/user/models"
-	"fmt"
+	"Virtual-Horizon/src/utils"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/golang-jwt/jwt"
+	"log"
 )
 
 func getRegisteredCourses(c *gin.Context) {
-	tokenString, err := c.Cookie("Authorization")
 
+	user, err := utils.GetUserFromToken(c)
 	if err != nil {
-		c.AbortWithStatus(http.StatusUnauthorized)
-	}
-
-	//Decode/Validate it
-	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok { //check the signing method
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return []byte(os.Getenv("SECRET")), nil
-	})
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		//check exp
-		if time.Now().Unix() > int64(claims["exp"].(float64)) {
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-
-		//find the user with token sub
-		var user usermodel.User
-		initializers.DB.First(&user, claims["sub"])
-		if user.ID == 0 {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"status":  "fail",
-				"message": " User Not found with this token",
-			},
-			)
-		}
-		var course models.Course
-		initializers.DB.Find(&course)
+		log.Println(err)
 	}
 
 }
--- backend/src/course/models/course_tutor_link.go
+++ backend/src/course/models/course_tutor_link.go
@@ -1,1 +1,16 @@
 package models
+
+import "time"
+
+type TutorCourseLink struct {
+	ID               uint      `json:"id" gorm:"primarykey"`
+	CourseID         uint      `json:"course_id"`
+	TutorID          uint      `json:"tutor_id"`
+	StartDate        time.Time `json:"start_date"`
+	EndDate          time.Time `json:"end_date"`
+	SessionFrequency string    `json:"session_frequency"`
+	MaxStudents      int       `json:"max_students"`
+	CurrentStudents  int       `json:"current_students"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+}
