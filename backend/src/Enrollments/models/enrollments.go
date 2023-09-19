5226e67e172062efbe068a70a0556d35d9bf81e5
vacaramin <vacaramin86@gmail.com>
2023-09-19 11:07:22 +0500

    enrollment API set

---
+++ backend/src/Enrollments/models/enrollments.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Enrollments struct {
+	ID             uint      `json:"enrollment_id"`
+	StudentID      uint      `json:"student_id"`
+	LinkID         uint      `json:"link_id"`
+	EnrollmentDate time.Time `json:"enrollment_date"`
+	Status         string    `json:"status"`
+	Grade          string    `json:"grade"`
+	Comment        string    `json:"comment"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
