1b1184886b5752ec8e49a04d42d6bfe1466fd0ba
vacaramin <vacaramin86@gmail.com>
2023-09-21 02:51:05 +0500

    Get Enrollments api complete

--- backend/src/tutor/models/tutor.go
+++ backend/src/tutor/models/tutor.go
@@ -1,14 +1,16 @@
 package models
 
 import (
+	"Virtual-Horizon/src/user/models"
 	"time"
 )
 
 type Tutor struct {
-	ID         uint      `gorm:"primarykey"`
-	Subject    string    `json:"subject"`
-	Experience string    `json:"experience"`
-	Rating     string    `json:"rating"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID         uint        `gorm:"primarykey"`
+	Subject    string      `json:"subject"`
+	Experience string      `json:"experience"`
+	Rating     string      `json:"rating"`
+	CreatedAt  time.Time   `json:"created_at"`
+	UpdatedAt  time.Time   `json:"updated_at"`
+	User       models.User `json:"user" gorm:"foreignKey:ID"`
 }
