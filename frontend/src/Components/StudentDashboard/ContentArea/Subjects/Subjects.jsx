aa01c47b96814fa4110be8b5ceccc96b0f62b976
vacaramin <vacaramin86@gmail.com>
2023-09-30 13:45:52 +0500

    Subjects removed from content area

---
+++ frontend/src/Components/StudentDashboard/ContentArea/Subjects/Subjects.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { useState } from "react";
+import MyPic from "../Home/MyPic.svg";
+
+
+function Subjects() {
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [subjectSelected, setSubjectSelected] = useState(false);
+
+  const subjects = [
+    {
+      name: "Math",
+      color: "#FF5B5B",
+      content: "Math Content",
+      teacher: {
+        name: "John Doe",
+        profilePic: MyPic,
+      },
+    },
+    {
+      name: "Science",
+      color: "#3EC1D3",
+      content: "Science Content",
+      teacher: {
+        name: "Jane Smith",
+        profilePic: MyPic,
+      },
+    },
+    {
+      name: "English",
+      color: "#FFC93C",
+      content: "English Content",
+      teacher: {
+        name: "John Doe",
+        profilePic: MyPic,
+      },
+    },
+    // Add more subjects with teacher info here
+  ];
+
+  const handleSubjectClick = (subject) => {
+    setSelectedSubject(subject);
+    setSubjectSelected(true);
+  };
+
+  const handleGoBack = () => {
+    setSelectedSubject(null);
+    setSubjectSelected(false);
+  };
+
+  if (subjectSelected && selectedSubject) {
+    return (
+      <div className="content-area-student">
+        <h1>{selectedSubject.name}</h1>
+        <p>{selectedSubject.content}</p>
+        <button onClick={handleGoBack}>Go Back</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>
+          <div className="content-area-student">
+            <div className="subject-container">
+              {subjects.map((subject) => (
+                <div
+                  onClick={() => handleSubjectClick(subject)}
+                  key={subject.name}
+                  className="subject-card"
+                  style={{
+                    backgroundColor: subject.color,
+                    width: "30%", // Adjust card width in percentage
+                    height: "200px", // Adjust card height in pixels
+                  }}
+                >
+                  <div className="subject-card-header">
+                    <div className="teacher-profile-pic">
+                      <img
+                        src={subject.teacher.profilePic}
+                        alt={subject.teacher.name}
+                      />
+                    </div>
+                    <div className="subject-header-details">
+                      <p>{subject.teacher.name}</p>
+                    </div>
+                  </div>
+                  <div className="subject-card-content">
+                    <p>{subject.name}</p>{" "}
+                    {/* Display subject name at the bottom center */}
+                    <p>{subject.content}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Subjects;
