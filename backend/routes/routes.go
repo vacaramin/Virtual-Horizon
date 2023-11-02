ad5dff2c0b50d18902399b272fe7072ac3671872
vacaramin <vacaramin86@gmail.com>
2023-11-02 19:04:49 +0500

    Enrollments interface done

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	enrollmentRoutes "Virtual-Horizon/src/Enrollments/routes"
+	"Virtual-Horizon/src/course/controllers"
 	courseRoutes "Virtual-Horizon/src/course/routes"
 	"Virtual-Horizon/src/middleware"
 	studentRoutes "Virtual-Horizon/src/student/routes"
@@ -13,11 +14,14 @@ import (
 func SetupRoutes(r *gin.Engine) {
 	// Add CORS middleware
 	r.Use(middleware.CorsMiddleware())
+	// Setting up controller structs for
+	courseController := &controllers.CourseController{}
 	// /user routes are being set in this route function
 	routes.SetupUserRoutes(r)
 	routes.SetupUserAuthRoutes(r)
 	studentRoutes.SetupStudentRoutes(r)
 	tutorRoutes.SetupRoutes(r)
-	courseRoutes.SetupRoutes(r)
+
+	courseRoutes.SetupRoutes(r, courseController)
 	enrollmentRoutes.SetupRoutes(r)
 }
