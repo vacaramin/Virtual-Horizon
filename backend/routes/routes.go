7f4bffabae32b360ae113e8fac5cc0b1f9dc6505
vacaramin <vacaramin86@gmail.com>
2023-11-02 23:15:25 +0500

    Student Interface added

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,27 +1,34 @@
 package routes
 
 import (
+	enrollmentControllers "Virtual-Horizon/src/Enrollments/controllers"
 	enrollmentRoutes "Virtual-Horizon/src/Enrollments/routes"
-	"Virtual-Horizon/src/course/controllers"
+	courseControllers "Virtual-Horizon/src/course/controllers"
 	courseRoutes "Virtual-Horizon/src/course/routes"
 	"Virtual-Horizon/src/middleware"
+	studentcontrollers "Virtual-Horizon/src/student/controllers"
 	studentRoutes "Virtual-Horizon/src/student/routes"
 	tutorRoutes "Virtual-Horizon/src/tutor/routes"
 	"Virtual-Horizon/src/user/routes"
 	"github.com/gin-gonic/gin"
 )
 
+// SetupRoutes This function calls all the Routes of the underlying MVC's
 func SetupRoutes(r *gin.Engine) {
 	// Add CORS middleware
 	r.Use(middleware.CorsMiddleware())
-	// Setting up controller structs for
-	courseController := &controllers.CourseController{}
-	// /user routes are being set in this route function
+
+	// Setting up controllers for the routes
+	courseController := &courseControllers.CourseController{}
+	enrollmentController := &enrollmentControllers.EnrollmentController{}
+	studentController := &studentcontrollers.StudentController{}
+	// user routes are being set in this route function
+	courseRoutes.SetupRoutes(r, courseController)
+	enrollmentRoutes.SetupRoutes(r, enrollmentController)
+	studentRoutes.SetupStudentRoutes(r, studentController, enrollmentController)
+
 	routes.SetupUserRoutes(r)
 	routes.SetupUserAuthRoutes(r)
-	studentRoutes.SetupStudentRoutes(r)
 	tutorRoutes.SetupRoutes(r)
 
-	courseRoutes.SetupRoutes(r, courseController)
-	enrollmentRoutes.SetupRoutes(r)
 }
--- backend/src/student/routes/student.routes.go
+++ backend/src/student/routes/student.routes.go
@@ -1,15 +1,18 @@
 package routes
 
 import (
-	controller "Virtual-Horizon/src/student/controllers"
+	EnrollmentController "Virtual-Horizon/src/Enrollments/controllers"
+	Studentcontroller "Virtual-Horizon/src/student/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 // SetupStudentRoutes this function sets All the User Routes with the baseUrl/user/`route`
-func SetupStudentRoutes(r *gin.Engine) {
+func SetupStudentRoutes(r *gin.Engine, studentController Studentcontroller.StudentFunctions, enrollmentController EnrollmentController.EnrollmentFunctions) {
 	StudentRoute := r.Group("/student")
-	StudentRoute.POST("/signup", controller.SignupStudent)
-	StudentRoute.POST("/vh-assistant", controller.GetVirtualAssistantResponse)
-	StudentRoute.POST("/getenrollments", controller.GetEnrollments)
+	StudentRoute.POST("/signup", studentController.SignupStudent)
+	StudentRoute.POST("/vh-assistant", studentController.GetVirtualAssistantResponse)
+	StudentRoute.POST("/getenrollments", func(c *gin.Context) {
+		studentController.GetEnrollments(c, enrollmentController)
+	})
 
 }
