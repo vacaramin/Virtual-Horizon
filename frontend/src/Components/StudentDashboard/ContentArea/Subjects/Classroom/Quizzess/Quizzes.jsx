610110538e33d7a40da1a452d265e15052f221b5
Awais Mohammad <94608299+awaismohammad23@users.noreply.github.com>
2023-11-09 13:00:32 +0500

    Quizzes cards added but the path for quiz is not retreving

--- frontend/src/Components/StudentDashboard/ContentArea/Subjects/Classroom/Quizzess/Quizzes.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/Subjects/Classroom/Quizzess/Quizzes.jsx
@@ -1,12 +1,48 @@
 import React from "react";
+import QuizCSS from "./Quizzes.module.css";
+import QuizPic from "./Quiz.svg"
 
 function Quizzes() {
-  // Add functionality for quizzes here
+  // Sample data for quizzes
+  const quizzesData = [
+    {
+      id: 1,
+      topic: "Topic 1",
+      description: "Description for Topic 1",
+      image: {QuizPic}, // or provide URL
+    },
+    {
+      id: 2,
+      topic: "Topic 2",
+      description: "Description for Topic 2",
+      image: {QuizPic}, // or provide URL
+    },
+    {
+      id: 3,
+      topic: "Topic 3",
+      description: "Description for Topic 3",
+      image: {QuizPic}, // or provide URL
+    },
+    // Add more quizzes as needed
+  ];
+
+  // Render quiz cards
+  const renderQuizCards = () => {
+    return quizzesData.map((quiz) => (
+      <div key={quiz.id} className={QuizCSS.quizCard}>
+        <img src={quiz.image} alt={`Topic ${quiz.id}`} />
+        <div className={QuizCSS.quizInfo}>
+          <h3>{quiz.topic}</h3>
+          <p>{quiz.description}</p>
+        </div>
+      </div>
+    ));
+  };
 
   return (
     <div>
       <h2>Quizzes</h2>
-      {/* Add quizzes UI and functionality here */}
+      <div className={QuizCSS.quizContainer}>{renderQuizCards()}</div>
     </div>
   );
 }
