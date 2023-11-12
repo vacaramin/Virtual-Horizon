0e1f1b980eb8b37b4c61dc0e6ed8255b36d6d34f
Awais Mohammad <94608299+awaismohammad23@users.noreply.github.com>
2023-11-12 14:22:31 +0500

    Quiz cards updated and quiz form added for grading

--- frontend/src/Components/StudentDashboard/ContentArea/Subjects/Classroom/Quizzess/Quizzes.jsx
+++ frontend/src/Components/StudentDashboard/ContentArea/Subjects/Classroom/Quizzess/Quizzes.jsx
@@ -1,48 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import QuizCSS from "./Quizzes.module.css";
-import QuizPic from "./Quiz.svg"
+import QuizPic from "./quizzes.svg";
 
 function Quizzes() {
-  // Sample data for quizzes
   const quizzesData = [
     {
       id: 1,
       topic: "Real and Complex Numbers",
       description: "Test for real and complex numbers.",
-      image: {QuizPic}, // or provide URL
+      image: "URL_TO_IMAGE_1", // Replace with actual image URL
     },
     {
       id: 2,
       topic: "Quadratic Equations",
       description: "Test for quadratic equations using different methods.",
-      image: {QuizPic}, // or provide URL
+      image: "URL_TO_IMAGE_2", // Replace with actual image URL
     },
     {
       id: 3,
       topic: "Trigonometry",
-      description: "Test for trignometric identities and their usage.",
-      image: {QuizPic}, // or provide URL
+      description: "Test for trigonometric identities and their usage.",
+      image: "URL_TO_IMAGE_3", // Replace with actual image URL
+    },
+    {
+      id: 4,
+      topic: "Algebra",
+      description: "Test for Algebraic equations.",
+      image: "URL_TO_IMAGE_4", // Replace with actual image URL
     },
     // Add more quizzes as needed
   ];
 
-  // Render quiz cards
-  const renderQuizCards = () => {
-    return quizzesData.map((quiz) => (
-      <div key={quiz.id} className={QuizCSS.quizCard}>
-        <img src={quiz.image} alt={`Topic ${quiz.id}`} />
-        <div className={QuizCSS.quizInfo}>
-          <h3>{quiz.topic}</h3>
-          <p>{quiz.description}</p>
+  const [showForm, setShowForm] = useState(false);
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
+
+  const handleQuizClick = (quiz) => {
+    setSelectedQuiz(quiz);
+    setShowForm(true);
+  };
+
+  const renderQuestionForm = () => {
+    if (showForm && selectedQuiz) {
+      return (
+        <div className={QuizCSS.questionForm}>
+          <h3>{selectedQuiz.topic} Questions</h3>
+          <form>
+            {/* Add your form fields */}
+            <label htmlFor="question1">Question 1:</label>
+            <input type="text" id="question1" name="question1" />
+            
+            <label htmlFor="question2">Question 2:</label>
+            <input type="text" id="question2" name="question2" />
+            
+            <button type="submit">Submit</button>
+          </form>
         </div>
-      </div>
-    ));
+      );
+    }
+    return null;
+  };
+
+  const renderQuizCards = () => {
+    if (!showForm) {
+      return quizzesData.map((quiz) => (
+        <div key={quiz.id} className={QuizCSS.quizCard} onClick={() => handleQuizClick(quiz)}>
+          <div className={QuizCSS.topBar} style={{ backgroundColor: "goldenrod" }}>
+            <img src={quiz.image} alt={`Topic ${quiz.id}`} />
+          </div>
+          <div className={QuizCSS.quizInfo}>
+            <h3>{quiz.topic}</h3>
+            <p>{quiz.description}</p>
+          </div>
+        </div>
+      ));
+    }
+    return null;
   };
 
   return (
     <div>
       <h2>Quizzes</h2>
-      <div className={QuizCSS.quizContainer}>{renderQuizCards()}</div>
+      <div className={QuizCSS.quizContainer}>
+        {renderQuizCards()}
+      </div>
+      {renderQuestionForm()}
     </div>
   );
 }
