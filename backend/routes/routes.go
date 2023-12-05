0d96c2e7f32f01df68647392b6273db97baec0d8
Waqar Amin <vacaramin86@gmail.com>
2023-12-05 11:04:15 +0500

    Notifications routes added

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -6,11 +6,16 @@ import (
 	courseControllers "Virtual-Horizon/src/course/controllers"
 	courseRoutes "Virtual-Horizon/src/course/routes"
 	"Virtual-Horizon/src/middleware"
+	notificationControllers "Virtual-Horizon/src/notifications/controllers"
+	notificationRoutes "Virtual-Horizon/src/notifications/routes"
+
 	studentcontrollers "Virtual-Horizon/src/student/controllers"
 	studentRoutes "Virtual-Horizon/src/student/routes"
 	tutorControllers "Virtual-Horizon/src/tutor/controllers"
 	tutorRoutes "Virtual-Horizon/src/tutor/routes"
+
 	"Virtual-Horizon/src/user/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,11 +29,13 @@ func SetupRoutes(r *gin.Engine) {
 	enrollmentController := &enrollmentControllers.EnrollmentController{}
 	studentController := &studentcontrollers.StudentController{}
 	tutorController := &tutorControllers.TutorController{}
+	notificationController := &notificationControllers.NotificationController{}
 	// user routes are being set in this route function
 	courseRoutes.SetupRoutes(r, courseController)
 	enrollmentRoutes.SetupRoutes(r, enrollmentController)
 	studentRoutes.SetupStudentRoutes(r, studentController, enrollmentController)
 	tutorRoutes.SetupRoutes(r, tutorController)
+	notificationRoutes.SetupRoutes(r, notificationController)
 
 	routes.SetupUserRoutes(r)
 	routes.SetupUserAuthRoutes(r)
